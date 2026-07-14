231a2c53d5023267876d08fd34960e9e0c6f5229
jhoanCampo94 <jhoansdvsf94@gmail.com>
2026-07-14 09:38:45 -0500

    Initial structure of the product registration form

---
+++ app/admin/products/new/page.tsx
@@ -0,0 +1,9 @@
+import ProductForm from "@/components/product/ProductForm";
+
+export default function Page() {
+  return(
+    <div className="space-y-8">
+      <ProductForm />
+    </div>
+  )
+}
---
+++ components/product/ProductForm.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@/components/ui/card"
+
+export default function ProductForm(){
+  return(
+    <div>
+      <h1 className="text-3xl font-bold tracking-tight">Crear producto</h1>
+      <p className="text-muted-foreground">Registra tu producto para que aparezca en la tienda!</p>
+      <Card className="mx-auto mt-8 max-w-4xl p-8">
+
+      </Card>
+    </div>
+  )
+}
